47e025fa99e3a0695e2a2bff54541d2cf06d34f8
koftov <ahronlu@gmail.com>
2020-12-09 01:10:27 +0200

    refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,11 @@ import { FETCH_POSTS, FETCH_USER } from "./types";
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, "userId"));
-  userIds.forEach((id) => dispatch(fetchUser(id)));
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async (dispatch) => {
